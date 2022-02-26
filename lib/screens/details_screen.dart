43f70d78dd3f2b705e90f169e82383275a287359
JoseLuisGit <joseluisstacruz@gmail.com>
2022-02-26 17:31:35 -0400

    add poster movie squeletor

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
 
-  
   @override
   Widget build(BuildContext context) {
        
@@ -15,10 +13,7 @@ class DetailsScreen extends StatelessWidget {
           SliverList(
             delegate: SliverChildListDelegate(
               [
-                Text('Tittle'),
-                SizedBox(
-                  height: 1000,
-                )
+                _PosterMovie()
               ]
             )
             )
@@ -53,6 +48,46 @@ class _CustomAppBar extends StatelessWidget {
           image: NetworkImage('https://via.placeholder.com/400x300'),
           fit: BoxFit.cover,
           ),
+      ),
+    );
+  }
+}
+
+class _PosterMovie extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final textTheme = Theme.of(context).textTheme;
+    return Container(
+      margin: const EdgeInsets.only(top: 5),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          const FadeInImage(
+            placeholder: AssetImage('assets/no-image.jpg'), 
+            image: NetworkImage('https://via.placeholder.com/300x400'),
+            height: 200,
+            ),
+
+            const SizedBox(
+              width: 10,
+            ),
+
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('movie.title', style: textTheme.headline6, overflow: TextOverflow.ellipsis, maxLines: 2,),
+                Text('movie.originalName', style: textTheme.subtitle1, overflow: TextOverflow.ellipsis,),
+                Row(
+                  children: [
+                    const Icon(Icons.star_border_outlined, size: 15,),
+                    Text('move.caption', style: textTheme.caption, overflow: TextOverflow.ellipsis,)
+                  ],
+                )
+              ],
+              )
+        ],
       ),
     );
   }
