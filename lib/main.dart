54a1386779c7bc3396156740a326540807387db1
Quispe <luis.quispe@a2odev.com>
2022-02-26 21:44:41 -0400

    add provider movies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/providers/movies_provider.dart';
+import 'package:provider/provider.dart';
+
 import 'package:movie_app/screens/screens.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppState());
+
+class AppState extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_)=> MoviesProvider(),lazy: false,
+        )
+      ],
+      child: MyApp(),
+
+      );
+  }
+}
 
 class MyApp extends StatelessWidget {
   @override
