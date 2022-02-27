2b8bd05c4fff2dc6e6bc7bce67dae3835dbf6a4a
Quispe <luis.quispe@a2odev.com>
2022-02-27 17:53:29 -0400

    improvements provider

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,15 +1,10 @@
-
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
 
 import 'package:movie_app/models/models.dart';
 import 'package:movie_app/models/popular_movies_response.dart';
 
-class MoviesProvider extends ChangeNotifier{
-
+class MoviesProvider extends ChangeNotifier {
   final _apiKey = 'a841e2c23fe58300a5d815470e79cdbe';
   final _language = 'es-ES';
   final _baseUrl = 'api.themoviedb.org';
@@ -17,41 +12,35 @@ class MoviesProvider extends ChangeNotifier{
   List<Movie> movies = [];
   List<Movie> popularMovies = [];
 
-  MoviesProvider(){
+  MoviesProvider() {
     getOnDisplayMovies();
     getOnPopularMovies();
   }
 
-
-  getOnDisplayMovies() async {
-    var url = Uri.https(_baseUrl, '3/movie/now_playing', {
-      'api_key' : _apiKey,
-      'language': _language,
-      'page' : '1'
-    });
+  Future<String> _getJsonData(String endpoint, [int page = 1]) async {
+    var url = Uri.https(_baseUrl, endpoint,
+        {'api_key': _apiKey, 'language': _language, 'page': '$page'});
 
     final response = await http.get(url);
+    return response.body;
+  }
 
-    NowPlayingResponse nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+  getOnDisplayMovies() async {
+    final jsonData = await _getJsonData('3/movie/now_playing');
+
+    NowPlayingResponse nowPlayingResponse =
+        NowPlayingResponse.fromJson(jsonData);
     movies = nowPlayingResponse.results;
 
     notifyListeners();
   }
 
-   getOnPopularMovies() async{
-      var url = Uri.https(_baseUrl, '3/movie/popular', {
-      'api_key' : _apiKey,
-      'language': _language,
-      'page' : '1'
-    });
-
-    final response = await http.get(url);
-
-    PopularMoviesResponse nowPlayingResponse = PopularMoviesResponse.fromJson(response.body);
-    popularMovies = [ ...popularMovies, ...nowPlayingResponse.results];
+  getOnPopularMovies() async {
+    final jsonData = await _getJsonData('3/movie/popular');
+    PopularMoviesResponse nowPlayingResponse =
+        PopularMoviesResponse.fromJson(jsonData);
+    popularMovies = [...popularMovies, ...nowPlayingResponse.results];
 
     notifyListeners();
-
-    }
-
+  }
 }
